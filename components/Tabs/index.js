e654c4ed42f336253a07b40219c72f3dd73bd0d1
alexisjcarr <acarr59@gmail.com>
2019-09-06 12:40:45 -0500

    ✨ Adds Tabs component.
     #1

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,19 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const tabs = document.querySelector(".topics");
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then(res => res.data.topics)
+  .then(topics => topics.forEach(topic => tabs.appendChild(Tab(topic))))
+  .catch(err => console.log(err));
+
+function Tab(arr) {
+  const tab = document.createElement("div");
+  tab.classList.add("tab");
+  tab.textContent = arr;
+
+  return tab;
+}
